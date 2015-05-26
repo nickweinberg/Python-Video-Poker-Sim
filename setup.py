6eb324f3b965d3299c969100eb5cbadba3b3a369
nick weinberg <nickweinberg90@gmail.com>
2015-05-25 19:51:11 -0700

    create formal project structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+# will set this up later
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    # config stuff here
+}
+
+# setup(**config)
---
+++ tests/test_video_poker_sim.py
@@ -0,0 +1,11 @@
+from nose2.tools import *
+import video_poker_sim
+
+def setup():
+    print "setup!"
+
+def teardown():
+    print "tear down"
+
+def test_basic():
+    print "basic test whatever"
